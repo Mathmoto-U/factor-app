a6d711c433a1fcf3f4e30a5269928432b7782ef5
Mathmoto-U <matsuyu2022@gmail.com>
2025-07-16 19:03:50 +0900

    Update expand3.py

--- expand3.py
+++ expand3.py
@@ -5,21 +5,24 @@ st.title("展開Level.3")
 
 if "b" not in st.session_state:
     st.session_state.b=random.choice([i for i in range(-15, 16) if i != 0])
+    st.session_state.X=random.choice(['x','y','z','a'])
 
 if st.button("新しい問題"):
     st.session_state.b=random.choice([i for i in range(-15, 16) if i != 0])
+    st.session_state.X=random.choice(['x','y','z','a','b','c','p','q'])
 b=st.session_state.b
+X=st.session_state.X
 
 def 定数(x):
-        return x
+        return f"{x}"
 
 def 根の積(y):
     if y>0:
-        return f"(x+{y})(x-{y})"
+        return f"({X}+{y})({X}-{y})"
     else:
-        return f"(x{y})(x+{-y})"
+        return f"({X}{y})({X}+{-y})"
 
-展開=f"x^2{定数(-b*b)}"
+展開=f"{X}^2{定数(-b*b)}"
 因数分解=根の積(b)
 
 st.markdown("次の式を展開せよ：")
