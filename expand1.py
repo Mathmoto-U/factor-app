c4ae96746adc86247107e1f7ab47a40d18195b40
Mathmoto-U <matsuyu2022@gmail.com>
2025-07-16 19:31:51 +0900

    Update expand1.py

--- expand1.py
+++ expand1.py
@@ -4,51 +4,52 @@ import random
 st.title("展開Level.1")
 
 if "b" not in st.session_state:
-    st.session_state.b=random.randint(-5,5)
-    st.session_state.c=random.randint(-5,5)
+    while True:
+        b, c = random.sample([i for i in range(-9, 10) if i != 0], 2)
+        if abs(b) != abs(c):
+            st.session_state.b = b
+            st.session_state.c = c
+            break
+    st.write(f"b = {st.session_state.b}, c = {st.session_state.c}")
+    st.session_state.X=random.choice(['x','y','z','a'])
 
 if st.button("新しい問題"):
-    st.session_state.b=random.randint(-5,5)
-    st.session_state.c=random.randint(-5,5)
+    while True:
+        b, c = random.sample([i for i in range(-9, 9) if i != 0], 2)
+        if abs(b) != abs(c):
+            st.session_state.b = b
+            st.session_state.c = c
+            break
+    st.session_state.X=random.choice(['x','x','x','x','x','x','y','z','a','b','c','p','q'])
 b=st.session_state.b
 c=st.session_state.c
+X=st.session_state.X
 
 def 一次(x):
     if x==1:
-        return "+x"
+        return f"+{X}"
     elif x==-1:
-        return "-x"
-    elif x==0:
-        return ""
+        return f"-{X}"
     elif x>0:
-        return f"+{x}x"
+        return f"+{x}{X}"
     else:
-        return f"{x}x"
+        return f"{x}{X}"
 def 定数(x):
     if x>0:
         return f"+{x}"
-    elif x==0:
-        return ""
     else:
         return f"{x}"
 
 def 根(y):
-    if y==0:
-        return f"x"
-    elif y>0:
-        return f"(x+{y})"
+    if y>0:
+        return f"({X}+{y})"
     else:
-        return f"(x{y})"
+        return f"({X}{y})"
 
 def 根の積(y,z):
-    if y==z==0:
-        return f"({根(y)})^2"
-    if y==z:
-        return f"{根(y)}^2"
-    else:
         return f"{根(y)}{根(z)}"
 
-展開=f"x^2{一次(b+c)}{定数(b*c)}"
+展開=f"{X}^2{一次(b+c)}{定数(b*c)}"
 因数分解=根の積(b,c)
 
 st.markdown("次の式を展開せよ：")
